53abafb47d6ac20d4a57310d62fa76512228bbb9
christopherchandrasaputra <13519074@std.stei.itb.ac.id>
2022-09-10 19:58:46 +0700

    Add expected output dimension length function

--- classes/misc/Function.py
+++ classes/misc/Function.py
@@ -4,5 +4,11 @@ conv2d_fpack = {
     'relu': lambda x: x * (x > 0),
 }
 
+## MISC FUNCTION PACK
+misc = {
+    'expected_output_dim_length':
+        lambda input, filter,
+            padding, stride: (input - filter + 2*padding)//stride + 1,
+}
 
 ## ... FUNCTION PACK
