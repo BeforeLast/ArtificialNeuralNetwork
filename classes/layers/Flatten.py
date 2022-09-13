f2196c50957a2ad436af7d5805d514779260d8f4
anakpindahan <akeylanaufal@gmail.com>
2022-09-13 13:17:50 +0700

    Add Flatten

--- classes/layers/Flatten.py
+++ classes/layers/Flatten.py
@@ -15,19 +15,48 @@ class Flatten(BaseLayer):
     algorithm:str = None
     input_shape:Union[int, tuple, list] = None
     ouput_shape:Union[int, tuple, list] = None
+    flatten_size:int = None
     
-    def __init__(self, ):
+    def __init__(self, input_shape):
         """
         Class constructor
         """
-        pass
-
+        if type(input_shape) is int:
+            if input_shape < 1:
+                raise ValueError('Input shape must be an integer not less than one')
+            else:
+                self.input_shape = (input_shape, input_shape)
+                self.size = input_shape * input_shape
+        elif (
+            (type(input_shape) is tuple or type(input_shape) is list)
+            and len(input_shape) == 2
+            and type(input_shape[0]) is int 
+            and type(input_shape[1]) is int
+        ):
+            if input_shape[0] < 1 or input_shape[1] < 1:
+                raise ValueError('Input size must be an integer not less than one')
+            else:
+                self.input_shape = input_shape
+                self.size = input_shape[0] * input_shape[1]
 
     def calculate(self, input):
         """
         Convert multi-dimensional input tensors into a single dimension
         """
-        pass
+        if type(input) is not list and type(input) is not tuple:
+            raise ValueError('Input must be a list or a tuple')
+        else:
+            calculate_input_shape = np.shape(input)
+            acceptable_input_shape = tuple(self.input_shape)
+            if calculate_input_shape != acceptable_input_shape:
+                raise ValueError('Input shape is not compatible. The input shape is {calculate_input_shape} \
+                                 but the acceptable size is {acceptable_input_shape}')
+            else:
+                flatted_layer = []
+                for i in range(len(input)):
+                    for j in range(len(input[0])):
+                        flatted_layer.append(input[i][j])
+                return flatted_layer
 
     def update(self):
         """
