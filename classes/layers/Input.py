d3a9c2fe86b8ad640bdfee641879f3ba7c3c2d53
christopherchandrasaputra <13519074@std.stei.itb.ac.id>
2022-09-17 12:34:03 +0700

    Refactor input layer

--- classes/layers/Input.py
+++ classes/layers/Input.py
@@ -15,22 +15,63 @@ class InputLayer(BaseLayer):
     output = None
     algorithm:str = None
     input_shape:Union[int, tuple, list] = None
-    ouput_shape:Union[int, tuple, list] = None
+    output_shape:Union[int, tuple, list] = None
     
-    def __init__(self, ):
+    def __init__(self, input_shape, **kwargs):
         """
         Class constructor
         """
-        pass
+        if type(input_shape) is int:
+            self.input_shape = (None, input_shape)
+        elif type(input_shape) in [list, tuple, np.ndarray]:
+            if input_shape[0] != None:
+                # Only state data dimension and channel
+                fix_shape = [None]
+                fix_shape.extend(list(input_shape))
+                self.input_shape = tuple(fix_shape)
+            else:
+                # Batch input shape already stated (None)
+                self.input_shape = tuple(input_shape)
+        else:
+            raise TypeError(f'Type {type(input_shape)} cannot be used as input \
+shape')
+        self.name = kwargs.get("name", "InputLayer")
 
-
-    def calculate(self, input):
+    def calculate(self, input) -> np.ndarray:
         """
-        Return input data as array from image folder
+        Check if input is the same as input_shape or not to prevent data shape
+        mismatch through the model and convert input to np.ndarray
         """
+        # Check input type
+        if type(input) not in [list, tuple, np.ndarray]:
+            raise TypeError(f"Unsuported input type for type {type(input)}")
+        # Save input history
+        self.input = input
+        # Convert input type to np.ndarray
+        output = np.array(input)
+        # Check input shape
+        if output.shape != self.input_shape[1:]:
+            raise ValueError(f'Mismatched input shape, {self.input_shape[1:]} \
+was expected and {output.shape} was given')
+        # Save output history
+        self.output = output.copy()
+        return output
 
-        data = ImageConvert().convert()
-        return data
+    def compile(self, input_shape=None):
+        """
+        COMPILING PURPOSE
+        Compile layer with the given input shape
+        """
+        # Calculate output shape
+        self.calculate_output_shape()
+
+    def calculate_output_shape(self):
+        """
+        COMPILING PURPOSE
+        Calculate ouput shape from layer's input shape
+        """
+        # Match output shape with input shape
+        self.output_shape = self.input_shape
 
     def update(self):
         """
