fix backprop

classes/layers/Conv2D.py
@@ -1,6 +1,7 @@
 # Guide : https://www.tensorflow.org/api_docs/python/tf/keras/layers/Conv2D
 
 from operator import index
+from classes.layers.Dense import Dense
 from classes.layers.Layer import Layer as BaseLayer
 from classes.misc.Function import conv2d_fpack, misc
 from scipy.signal import fftconvolve
@@ -201,18 +202,44 @@ of two integers")
 
             curr_deltas_wrt_inputs = [np.zeros(self.input_shape[-3:-1])]                    
             curr_deltas_wrt_filters = []
-            next_layer_delta = next_layer.deltas_wrt_inputs[i]
+
+            next_layer_delta = None
+            if (type(next_layer) is Conv2D) :
+                # Next layer is conv
+                next_layer_delta = next_layer.deltas_wrt_inputs[i]
+            else :
+                # Next layer is dense
+                next_layer_delta = []
+
+                for weight in next_layer.weights:
+                    curr = np.dot(next_layer.deltas_wrt_inputs, weight)
+                    next_layer_delta.append(curr)
+            
             for channel_idx in range(self.input_shape[-1]):
-                curr_deltas_wrt_output = delta[channel_idx].T @ next_layer_delta
-                curr_deltas_wrt_filter = fftconvolve(
-                    input[:,:,j], curr_deltas_wrt_output, mode='valid'
-                )
-                curr_deltas_wrt_inputs = fftconvolve(
-                    np.rot90(self.conv_filters[i][0][channel_idx], 2), curr_deltas_wrt_output, mode='full'
-                )
-                curr_deltas_wrt_filters.append(curr_deltas_wrt_filter)
-                self.deltas_wrt_inputs[channel_idx] = self.deltas_wrt_inputs[channel_idx] + curr_deltas_wrt_inputs
+                if (type(next_layer) is Conv2D):
+                    # Next layer is conv
+                    curr_deltas_wrt_output = delta[channel_idx].T @ next_layer_delta
+                    curr_deltas_wrt_filter = fftconvolve(
+                        input[:,:,j], curr_deltas_wrt_output, mode='valid'
+                    )
+                    curr_deltas_wrt_inputs = fftconvolve(
+                        np.rot90(self.conv_filters[i][0][channel_idx], 2), curr_deltas_wrt_output, mode='full'
+                    )
+                    curr_deltas_wrt_filters.append(curr_deltas_wrt_filter)
+
+                    self.deltas_wrt_inputs[channel_idx] = self.deltas_wrt_inputs[channel_idx] + curr_deltas_wrt_inputs
+                else :
+                    # Next layer is dense
+                    delta_cross_input = []
+                    for idx, delta in enumerate(next_layer_delta):
+                        curr = [delta * next_layer.output[idx-1]]
+                        delta_cross_input.append(curr)
+
+                    curr_deltas_wrt_filters.append(delta_cross_input)
+
             self.deltas_wrt_filters.append(curr_deltas_wrt_filters)
+
+        self.deltas_wrt_filters = np.asarray(self.deltas_wrt_filters)
 
     def update(self, learning_rate):
         """
@@ -221,8 +248,8 @@ of two integers")
         # Update filter weight
         for i in range(self.num_of_filters):
             for channel_idx in range(self.input_shape[-1]):
-                delta_weight = self.deltas_wrt_filters * learning_rate
-                self.conv_filters[i][0][channel_idx] = self.conv_filters[i][0][channel_idx] + delta_weight            
+                delta_weight = self.deltas_wrt_filters[i][channel_idx] * learning_rate
+                self.conv_filters[i][0][channel_idx] = self.conv_filters[i][0][channel_idx] - delta_weight            
     
     # DONE : FIX CONVOLVE
     def convolve(self, input):

classes/layers/Dense.py
@@ -19,7 +19,7 @@ class Dense(BaseLayer):
     output_shape:tuple[None, int] = None
     num_of_units:int = None
     weights:np.ndarray = None
-    deltas:np.ndarray = None
+    deltas_wrt_inputs:np.ndarray = None
     
     def __init__(self, units, activation='relu', **kwargs):
         """
@@ -124,21 +124,21 @@ but {np.array(input).shape} shape was given.')
         # Update deltas
         if (target == None):
             # Hidden layer
-            next_delta = np.dot(next_layer.deltas, next_layer.weights[1:].T)
-            self.deltas = self.output_deriv * next_delta
+            next_delta = np.dot(next_layer.deltas_wrt_inputs, next_layer.weights[1:].T)
+            self.deltas_wrt_inputs = self.output_deriv * next_delta
             pass
         else :
             # Output layer
             errors = dense_epack_deriv[self.algorithm](target, self.output)
-            self.deltas = self.output_deriv * errors
+            self.deltas_wrt_inputs = self.output_deriv * errors
 
     def update(self, learning_rate):
         """
         Update the layer's weight
         """
         # Update weights
-        delta_weight = np.dot(self.output, self.deltas) * learning_rate
-        self.weights = self.weights + delta_weight
+        delta_weight = np.dot(self.output, self.deltas_wrt_inputs) * learning_rate
+        self.weights = self.weights - delta_weight
 
     def to_object(self):
         """

classes/layers/Flatten.py
@@ -15,6 +15,8 @@ class Flatten(BaseLayer):
     algorithm:str = None
     input_shape:tuple = None
     output_shape:tuple[None, int] = None
+    weights:np.ndarray = None 
+    deltas_wrt_inputs:np.ndarray = None
     
     def __init__(self, **kwargs):
         """
@@ -27,6 +29,7 @@ class Flatten(BaseLayer):
         """
         Convert multi-dimensional input tensors into a single dimension
         """
+        self.output = input.flatten()
         return input.flatten()
 
     def compile(self, input_shape):
@@ -57,7 +60,8 @@ class Flatten(BaseLayer):
         ! IGNORE !
         Does not exist for this layer
         """
-        pass
+        self.weights = next_layer.weights
+        self.deltas_wrt_inputs = next_layer.deltas_wrt_inputs
 
     def update(self, learning_rate):
         """